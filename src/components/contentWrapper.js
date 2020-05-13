b05bbbeebaeceb0b5d2e5384c093ebe7d2276a9c
Yogi Saputro <yogitn19@gmail.com>
2020-05-13 10:16:14 +0700

    display top songs last.fm

--- src/components/contentWrapper.js
+++ src/components/contentWrapper.js
@@ -1,14 +1,39 @@
 import React from 'react';
+import axios from 'axios';
 
 class ContentWrapper extends React.Component {
+    state = {
+        songs: []
+    };
+
+    componentDidMount() {
+        axios.get('http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=bb4df6405c3919b1c2dd3ae50959afbb&format=json')
+        .then(response => {
+            this.setState({songs: response.data.tracks.track});
+        })
+        .catch(err => {
+            console.error(err); 
+        })
+    }
+
     render()  {
         return(
             <div className="content">
-            <h2 className="content-head is-center">Excepteur sint occaecat cupidatat.</h2>
+            <h2 className="content-head is-center">Top {this.state.songs.length} Songs on Last.fm</h2>
     
             <div className="pure-g">
                 <div className="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
-    
+                    {this.state.songs.map((song, index) => 
+                    <div>
+                        <h3 className="content-subhead">
+                            {index+1}. {song.name}
+                        </h3>
+                        <p>played {song.playcount} times</p>
+                        <p>by {song.artist.name}</p>
+                    
+                    </div>
+                    )}
+
                     <h3 className="content-subhead">
                         <i className="fa fa-rocket"></i>
                         Get Started Quickly
--- src/components/splashContainer.js
+++ src/components/splashContainer.js
@@ -5,12 +5,9 @@ class SplashContainer extends React.Component {
         return(
             <div className="splash-container">
                 <div className="splash">
-                    <h1 className="splash-head">Big Bold Text</h1>
+                    <h1 className="splash-head">TOP Chart 2020</h1>
                     <p className="splash-subhead">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    </p>
-                    <p>
-                        <a href="http://purecss.io" className="pure-button pure-button-primary">Get Started</a>
+                        Versi Last.fm
                     </p>
                 </div>
             </div>
